909d34636d1612b32d97a924e7fe6c55aad7c582
Karthik <karthikk1162@gmail.com>
2025-12-13 16:11:23 +0530

    Configuration made

--- cyber_hand.py
+++ cyber_hand.py
@@ -3,3 +3,10 @@ import mediapipe as mp
 import numpy as np
 import math
 from collections import deque
+
+# "Cyberpunk" Palette (Darker, grittier colors)
+C_VOID = (15, 15, 20)
+C_CORE = (0, 255, 255)
+C_FLUX = (0, 100, 255)
+C_SCAN = (255, 50, 50)
+C_TEXT = (200, 200, 200)
